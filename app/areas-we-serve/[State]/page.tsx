1fe4cbc1d4b0d5bfcf7f04394afb4f837d1317e8
Satnam-Gill <ersatnamgill@gmail.com>
2025-04-14 13:39:30 +0530

    (Update: Metas and Content )

--- app/areas-we-serve/[State]/page.tsx
+++ app/areas-we-serve/[State]/page.tsx
@@ -35,7 +35,7 @@ export function generateMetadata({ params }: SubdomainPageProps) {
   const ContentData = cityData[State];
   return {
     title: ContentData?.metaTitle,
-    description: ContentData?.metaDescription,
+    description: `${ContentData?.metaDescription.replace("Call us today!",`Call us at ${ContactInfo.No}`)}.`,
     alternates: {
       canonical: `https://${ContactInfo.host}/areas-we-serve/${State}`,
     },
@@ -150,7 +150,7 @@ export default function SubdomainPage({ params }: SubdomainPageProps) {
         h1={`${ContentData.h1Banner} ${ContentData.zipCodes && ContentData.zipCodes.split("|")[0]}`}
         image={ContentData.bannerImage}
         header={ContentData.bannerQuote}
-        p1={ContentData.metaDescription}
+        p1={`${ContentData?.metaDescription.replace("Call us today!",`Call us at ${ContactInfo.No}`)}.`}
       />
       {/* Section 1 */}
       {/* <p>{subDomain.map((item:any)=>(
